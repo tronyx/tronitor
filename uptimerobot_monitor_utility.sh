bae92250d1ebd6ae9eebc5b187404822e036dac8
Yocum, Chris <chryo12@yahoo.com>
2018-10-09 17:04:31 -0400

    Testing

--- uptimerobot_monitor_utility.sh
+++ uptimerobot_monitor_utility.sh
@@ -557,8 +557,7 @@ get_info() {
 get_alert_contacts() {
   echo 'The following alert contacts have been found for your UptimeRobot account:'
   echo ''
-  curl -s -X POST "${apiUrl}"getAlertContacts -d "api_key=${apiKey}" -d "format=json"
-  #jq
+  curl -s -X POST "${apiUrl}"getAlertContacts -d "api_key=${apiKey}" -d "format=json" |jq -e
   echo ''
 }
 
