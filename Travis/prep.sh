e61a50ddbe11b813afa577bb5ed0f0640e4bffb0
Yocum, Chris <chryo12@yahoo.com>
2018-10-09 18:19:28 -0400

    Testing

--- Travis/prep.sh
+++ Travis/prep.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 #
-set -eo pipefail
 
 curl -s -X POST "${apiUrl}"newMonitor -d @travisone.json --header "Content-Type: application/json"
 curl -s -X POST "${apiUrl}"newMonitor -d @travistwo.json --header "Content-Type: application/json"
