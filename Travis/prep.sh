064f5bef8034f2d4be5df2405f99c27bc9db39df
Yocum, Chris <chryo12@yahoo.com>
2018-10-10 15:26:27 -0400

    Testing

--- Travis/prep.sh
+++ Travis/prep.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 #
-curl -X POST -H "Cache-Control: no-cache" -H "Content-Type: application/x-www-form-urlencoded" -d "api_key=${travisApiKey}&format=json&type=1&url=https://google.co.uk&friendly_name=TravisOne" "https://api.uptimerobot.com/v2/newMonitor"
+curl -s -X POST -H "Cache-Control: no-cache" -H "Content-Type: application/x-www-form-urlencoded" -d "api_key=${travisApiKey}&format=json&type=1&url=https://google.co.uk&friendly_name=TravisOne" "https://api.uptimerobot.com/v2/newMonitor"
 
-curl -X POST -H "Cache-Control: no-cache" -H "Content-Type: application/x-www-form-urlencoded" -d "api_key=${travisApiKey}&format=json&type=1&url=https://amazon.co.uk&friendly_name=TravisTwo" "https://api.uptimerobot.com/v2/newMonitor" 
+curl -s -X POST -H "Cache-Control: no-cache" -H "Content-Type: application/x-www-form-urlencoded" -d "api_key=${travisApiKey}&format=json&type=1&url=https://amazon.com&friendly_name=TravisTwo" "https://api.uptimerobot.com/v2/newMonitor" 
