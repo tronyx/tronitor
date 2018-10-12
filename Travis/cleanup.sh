50823840063b895eab4dcf189cb1b558e3c9fb09
Chris Yocum <chryo12@yahoo.com>
2018-10-12 14:35:51 -0400

    Add sudo bash

--- Travis/cleanup.sh
+++ Travis/cleanup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #
-./Travis/travis_uptimerobot_monitor_utility.sh -d 'travisone'
-./Travis/travis_uptimerobot_monitor_utility.sh -d 'travistwo'
-./Travis/travis_uptimerobot_monitor_utility.sh -d 'googlehttp'
-./Travis/travis_uptimerobot_monitor_utility.sh -d 'googleping'
-./Travis/travis_uptimerobot_monitor_utility.sh -d 'googleport'
-./Travis/travis_uptimerobot_monitor_utility.sh -d 'googlekeyword'
+sudo bash ./Travis/travis_uptimerobot_monitor_utility.sh -d 'travisone'
+sudo bash ./Travis/travis_uptimerobot_monitor_utility.sh -d 'travistwo'
+sudo bash ./Travis/travis_uptimerobot_monitor_utility.sh -d 'googlehttp'
+sudo bash ./Travis/travis_uptimerobot_monitor_utility.sh -d 'googleping'
+sudo bash ./Travis/travis_uptimerobot_monitor_utility.sh -d 'googleport'
+sudo bash ./Travis/travis_uptimerobot_monitor_utility.sh -d 'googlekeyword'
