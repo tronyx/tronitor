a78cad882a0d546dc45c90b95610fb5148e828bd
Chris Yocum <chryo12@yahoo.com>
2019-03-28 01:50:57 -0400

    Minor updates

--- Travis/Scripts/shfmt.sh
+++ Travis/Scripts/shfmt.sh
@@ -2,5 +2,5 @@
 #
 sudo wget https://raw.githubusercontent.com/jamesmstone/dockerfiles/master/shfmt/Dockerfile
 sudo docker build -t jamesmstone/shfmt .
-sudo docker run -it --rm -v /home/travis/build/christronyxyocum/tronitor/Travis/Scripts:/sh -w /sh jamesmstone/shfmt -s -i 4 -ci -sr -d travis_usmu.sh
-sudo docker run -it --rm -v /home/travis/build/christronyxyocum/tronitor:/sh -w /sh jamesmstone/shfmt -s -i 4 -ci -sr -d usmu.sh
+sudo docker run -it --rm -v /home/travis/build/christronyxyocum/tronitor/Travis/Scripts:/sh -w /sh jamesmstone/shfmt -s -i 4 -ci -sr -d travis_tronitor.sh
+sudo docker run -it --rm -v /home/travis/build/christronyxyocum/tronitor:/sh -w /sh jamesmstone/shfmt -s -i 4 -ci -sr -d tronitor.sh
