51f181d29bec5e901b59e35072c7814231e4b20b
Chris Yocum <chryo12@yahoo.com>
2019-03-29 00:49:24 -0400

    Fixes

--- Travis/Scripts/hc_prep.sh
+++ Travis/Scripts/hc_prep.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-curl -s -X POST https://healthchecks.io/api/v1/checks/ "X-Api-Key: ${travisHCApiKey}" \
+curl -s -X POST https://healthchecks.io/api/v1/checks/ -H "X-Api-Key: ${travisHCApiKey}" \
     --data '{"name": "TravisOne", "tags": "prod www", "timeout": 3600, "grace": 60}'
 
-curl -s -X POST https://healthchecks.io/api/v1/checks/ "X-Api-Key: ${travisHCApiKey}" \
+curl -s -X POST https://healthchecks.io/api/v1/checks/ -H "X-Api-Key: ${travisHCApiKey}" \
     --data '{"name": "TravisTwo", "tags": "prod www", "timeout": 3600, "grace": 60}'
 
-curl -s -X POST https://healthchecks.io/api/v1/checks/ "X-Api-Key: ${travisHCApiKey}" \
+curl -s -X POST https://healthchecks.io/api/v1/checks/ -H "X-Api-Key: ${travisHCApiKey}" \
     --data '{"name": "TravisThree", "tags": "prod www", "timeout": 3600, "grace": 60}'
