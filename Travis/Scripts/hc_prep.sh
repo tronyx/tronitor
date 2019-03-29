a4f686c0173a4b97e276ef8de927780408f94f8e
Chris Yocum <chryo12@yahoo.com>
2019-03-28 21:11:53 -0400

    Fixes

--- Travis/Scripts/hc_prep.sh
+++ Travis/Scripts/hc_prep.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#
+
 curl -s -X POST https://healthchecks.io/api/v1/checks/ \
     --data '{"api_key": '"${travisHCApiKey}"', "name": "TravisOne", "tags": "prod www", "timeout": 3600, "grace": 60}'
 
--- Travis/Scripts/sc_prep.sh
+++ Travis/Scripts/sc_prep.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#
+
 curl -s -H "Cache-Control: no-cache" -H "Content-Type: application/x-www-form-urlencoded" -H "API: ${travisSCApiKey}" -H "Username: ${travisSCUsername}" -d "WebsiteName=TravisOne&WebsiteURL=https://google.co.uk&CheckRate=300&TestType=HTTP" -X PUT https://app.statuscake.com/API/Tests/Update
 
 curl -s -H "Cache-Control: no-cache" -H "Content-Type: application/x-www-form-urlencoded" -H "API: ${travisSCApiKey}" -H "Username: ${travisSCUsername}" -d "WebsiteName=TravisTwo&WebsiteURL=http://chrisyocumissuperawesome.com&CheckRate=300&TestType=HTTP" -X PUT https://app.statuscake.com/API/Tests/Update
