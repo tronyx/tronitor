d8dae14257120e2a327352275532616f931ed379
Chris Yocum <chryo12@yahoo.com>
2021-02-24 09:23:33 -0500

    Changes for using GH org

--- tronitor.sh
+++ tronitor.sh
@@ -61,7 +61,8 @@ hcProviderStatus='invalid'
 upProviderStatus='invalid'
 # Set initial SC username status.
 scUsernameStatus='invalid'
-# Set initial GH username status.
+# Set initial GH repo owner and username status.
+ghRepoOwnerStatus='invalid'
 ghUsernameStatus='invalid'
 # Set initial Upptime repo status.
 upRepoStatus='invalid'
@@ -347,26 +348,28 @@ check_curl() {
 # Function to grab line numbers of the user-defined and status variables.
 get_line_numbers() {
     # Line numbers for user-defined variables.
-    scUsernameLineNum=$(head -67 "${scriptname}" | grep -En -A1 'specify your username' | tail -1 | awk -F- '{print $1}')
-    ghUsernameLineNum=$(head -67 "${scriptname}" | grep -En 'gitHubUser' | awk -F: '{print $1}')
-    upRepoLineNum=$(head -67 "${scriptname}" | grep -En 'upptimeRepo' | awk -F: '{print $1}')
-    urApiKeyLineNum=$(head -67 "${scriptname}" | grep -En -A4 'Specify API key' | grep 'ur' | awk -F- '{print $1}')
-    scApiKeyLineNum=$(head -67 "${scriptname}" | grep -En -A4 'Specify API key' | grep 'sc' | awk -F- '{print $1}')
-    hcApiKeyLineNum=$(head -67 "${scriptname}" | grep -En -A4 'Specify API key' | grep 'hc' | awk -F- '{print $1}')
-    ghTokenLineNum=$(head -67 "${scriptname}" | grep -En -A4 'Specify API key' | grep 'gh' | awk -F- '{print $1}')
-    webhookUrlLineNum=$(head -67 "${scriptname}" | grep -En -A1 'Discord/Slack' | tail -1 | awk -F- '{print $1}')
+    scUsernameLineNum=$(head -68 "${scriptname}" | grep -En -A1 'specify your username' | tail -1 | awk -F- '{print $1}')
+    ghRepoOwnerLineNum=$(head -68 "${scriptname}" | grep -En 'repoOwner' | awk -F: '{print $1}')
+    ghUsernameLineNum=$(head -68 "${scriptname}" | grep -En 'gitHubUser' | awk -F: '{print $1}')
+    upRepoLineNum=$(head -68 "${scriptname}" | grep -En 'upptimeRepo' | awk -F: '{print $1}')
+    urApiKeyLineNum=$(head -68 "${scriptname}" | grep -En -A4 'Specify API key' | grep 'ur' | awk -F- '{print $1}')
+    scApiKeyLineNum=$(head -68 "${scriptname}" | grep -En -A4 'Specify API key' | grep 'sc' | awk -F- '{print $1}')
+    hcApiKeyLineNum=$(head -68 "${scriptname}" | grep -En -A4 'Specify API key' | grep 'hc' | awk -F- '{print $1}')
+    ghTokenLineNum=$(head -68 "${scriptname}" | grep -En -A4 'Specify API key' | grep 'gh' | awk -F- '{print $1}')
+    webhookUrlLineNum=$(head -68 "${scriptname}" | grep -En -A1 'Discord/Slack' | tail -1 | awk -F- '{print $1}')
     # Line numbers for status variables.
-    urApiStatusLineNum=$(head -67 "${scriptname}" | grep -En -A4 'Set initial API key' | grep 'ur' | awk -F- '{print $1}')
-    scApiStatusLineNum=$(head -67 "${scriptname}" | grep -En -A4 'Set initial API key' | grep 'sc' | awk -F- '{print $1}')
-    hcApiStatusLineNum=$(head -67 "${scriptname}" | grep -En -A4 'Set initial API key' | grep 'hc' | awk -F- '{print $1}')
-    ghTokenStatusLineNum=$(head -67 "${scriptname}" | grep -En -A4 'Set initial API key' | grep 'gh' | awk -F- '{print $1}')
-    urProviderStatusLineNum=$(head -67 "${scriptname}" | grep -En -A4 'Set initial provider status' | grep 'ur' | awk -F- '{print $1}')
-    scProviderStatusLineNum=$(head -67 "${scriptname}" | grep -En -A4 'Set initial provider status' | grep 'sc' | awk -F- '{print $1}')
-    hcProviderStatusLineNum=$(head -67 "${scriptname}" | grep -En -A4 'Set initial provider status' | grep 'hc' | awk -F- '{print $1}')
-    upProviderStatusLineNum=$(head -67 "${scriptname}" | grep -En -A4 'Set initial provider status' | grep 'up' | awk -F- '{print $1}')
-    scUserStatusLineNum=$(head -67 "${scriptname}" | grep -En -A1 'Set initial SC username status' | tail -1 | awk -F- '{print $1}')
-    ghUserStatusLineNum=$(head -67 "${scriptname}" | grep -En -A1 'Set initial GH username status' | tail -1 | awk -F- '{print $1}')
-    upRepoStatusLineNum=$(head -67 "${scriptname}" | grep -En -A1 'Set initial Upptime repo status' | tail -1 | awk -F- '{print $1}')
+    urApiStatusLineNum=$(head -68 "${scriptname}" | grep -En -A4 'Set initial API key' | grep 'ur' | awk -F- '{print $1}')
+    scApiStatusLineNum=$(head -68 "${scriptname}" | grep -En -A4 'Set initial API key' | grep 'sc' | awk -F- '{print $1}')
+    hcApiStatusLineNum=$(head -68 "${scriptname}" | grep -En -A4 'Set initial API key' | grep 'hc' | awk -F- '{print $1}')
+    ghTokenStatusLineNum=$(head -68 "${scriptname}" | grep -En -A4 'Set initial API key' | grep 'gh' | awk -F- '{print $1}')
+    urProviderStatusLineNum=$(head -68 "${scriptname}" | grep -En -A4 'Set initial provider status' | grep 'ur' | awk -F- '{print $1}')
+    scProviderStatusLineNum=$(head -68 "${scriptname}" | grep -En -A4 'Set initial provider status' | grep 'sc' | awk -F- '{print $1}')
+    hcProviderStatusLineNum=$(head -68 "${scriptname}" | grep -En -A4 'Set initial provider status' | grep 'hc' | awk -F- '{print $1}')
+    upProviderStatusLineNum=$(head -68 "${scriptname}" | grep -En -A4 'Set initial provider status' | grep 'up' | awk -F- '{print $1}')
+    scUserStatusLineNum=$(head -68 "${scriptname}" | grep -En -A1 'Set initial SC username status' | tail -1 | awk -F- '{print $1}')
+    ghUserStatusLineNum=$(head -68 "${scriptname}" | grep -En -A2 'Set initial GH repo owner and username status' | grep ghUser | awk -F- '{print $1}')
+    ghRepoOwnerStatusLineNum=$(head -68 "${scriptname}" | grep -En -A2 'Set initial GH repo owner and username status' | grep RepoOwner | awk -F- '{print $1}')
+    upRepoStatusLineNum=$(head -68 "${scriptname}" | grep -En -A1 'Set initial Upptime repo status' | tail -1 | awk -F- '{print $1}')
 }
 
 # Function for catching when a curl or jq command fails to display a message and
@@ -449,7 +452,8 @@ check_provider() {
         readonly apiUrl="https://${healthchecksDomain}/api/v1/"
     elif [[ ${providerName} == 'upptime' ]]; then
         readonly apiUrl='https://api.github.com/'
-        upRawURL="https://raw.githubusercontent.com/${gitHubUsername}/${upptimeRepo}/"
+        #upRawURL="https://raw.githubusercontent.com/${gitHubUsername}/${upptimeRepo}/"
+        upRawURL="https://raw.githubusercontent.com/${repoOwner}/${upptimeRepo}/"
     fi
 }
 
@@ -507,7 +511,7 @@ check_sc_creds() {
 
 # Function to check that the provided GitHub username and PAT are valid
 check_gh_creds() {
-    while [[ ${ghUsernameStatus} == 'invalid' ]] || [[ ${ghTokenStatus} == 'invalid' ]]; do
+    while [[ ${ghRepoOwnerStatus} == 'invalid' ]] || [[ ${ghUsernameStatus} == 'invalid' ]] || [[ ${ghTokenStatus} == 'invalid' ]]; do
         if [[ -z ${ghToken} ]]; then
             echo -e "${red}You didn't define your ${providerName^} PAT in the script!${endColor}"
             echo ''
@@ -517,6 +521,16 @@ check_gh_creds() {
             echo ''
             sed -i "${ghTokenLineNum} s/ghToken='[^']*'/ghToken='${API}'/" "${scriptname}"
             ghToken="${API}"
+        elif [[ -z ${repoOwner} ]]; then
+            echo -e "${red}You didn't specify the owner of your Upptime repository in the script!${endColor}"
+            echo ''
+            echo "Enter your Upptime repo owner:"
+            read -r owner
+            echo ''
+            echo ''
+            sed -i "${ghRepoOwnerLineNum} s/repoOwner='[^']*'/repoOwner='${owner}'/" "${scriptname}"
+            repoOwner="${owner}"
+            upRawURL="https://raw.githubusercontent.com/${repoOwner}/${upptimeRepo}/"
         elif [[ -z ${gitHubUsername} ]]; then
             echo -e "${red}You didn't specify your GitHub username in the script!${endColor}"
             echo ''
@@ -526,7 +540,7 @@ check_gh_creds() {
             echo ''
             sed -i "${ghUsernameLineNum} s/gitHubUsername='[^']*'/gitHubUsername='${username}'/" "${scriptname}"
             gitHubUsername="${username}"
-            upRawURL="https://raw.githubusercontent.com/${gitHubUsername}/${upptimeRepo}/"
+            #upRawURL="https://raw.githubusercontent.com/${gitHubUsername}/${upptimeRepo}/"
         else
             ghStatus=$(curl -s -XGET -H "Authorization: bearer ${ghToken}" "${apiUrl}"user | jq -r .login)
             ghStatus=$(echo "${ghStatus}" | awk '{print tolower($0)}')
@@ -545,9 +559,11 @@ check_gh_creds() {
                 echo ''
                 sed -i "${ghUsernameLineNum} s/gitHubUsername='[^']*'/gitHubUsername='${username}'/" "${scriptname}"
                 gitHubUsername="${username}"
-                upRawURL="https://raw.githubusercontent.com/${gitHubUsername}/${upptimeRepo}/"
+                upRawURL="https://raw.githubusercontent.com/${repoOwner}/${upptimeRepo}/"
             elif [[ ${ghStatus} == "${gitHubUsername}" ]]; then
-                echo 'Validating that the provided GitHub username and PAT are functional...'
+                echo 'Validating that the provided Upptime repo owner, GitHub username, and PAT are functional...'
+                sed -i "${ghRepoOwnerStatusLineNum} s/ghRepoOwnerStatus='[^']*'/ghRepoOwnerStatus='ok'/" "${scriptname}"
+                ghRepoOwnerStatus='ok'
                 sed -i "${ghTokenStatusLineNum} s/ghTokenStatus='[^']*'/ghTokenStatus='ok'/" "${scriptname}"
                 ghTokenStatus='ok'
                 sed -i "${ghUserStatusLineNum} s/ghUsernameStatus='[^']*'/ghUsernameStatus='ok'/" "${scriptname}"
@@ -571,10 +587,10 @@ check_up_repo() {
             echo ''
             sed -i "${upRepoLineNum} s/upptimeRepo='[^']*'/upptimeRepo='${repo}'/" "${scriptname}"
             upptimeRepo="${repo}"
-            upRawURL="https://raw.githubusercontent.com/${gitHubUsername}/${upptimeRepo}/"
+            upRawURL="https://raw.githubusercontent.com/${repoOwner}/${upptimeRepo}/"
         else
             echo 'Validating that the provided Upptime repository is functional...'
-            status=$(curl -w "%{http_code}\n" -sI -o /dev/null https://github.com/"${gitHubUsername}"/"${upptimeRepo}"/) || fatal
+            status=$(curl -w "%{http_code}\n" -sI -o /dev/null https://github.com/"${repoOwner}"/"${upptimeRepo}"/) || fatal
 
             if [[ ${status} != '200' ]]; then
                 echo -e "${red}The Upptime repository that you provided does not appear to be valid!${endColor}"
@@ -583,7 +599,7 @@ check_up_repo() {
             elif [[ ${status} == '200' ]]; then
                 sed -i "${upRepoStatusLineNum} s/upRepoStatus='[^']*'/upRepoStatus='ok'/" "${scriptname}"
                 upRepoStatus='ok'
-                upRawURL="https://raw.githubusercontent.com/${gitHubUsername}/${upptimeRepo}/"
+                upRawURL="https://raw.githubusercontent.com/${repoOwner}/${upptimeRepo}/"
                 echo -e "${grn}Success!${endColor}"
                 echo ''
             fi
@@ -711,7 +727,7 @@ get_data() {
     elif [[ ${providerName} == 'healthchecks' ]]; then
         curl --fail -s -H "X-Api-Key: ${apiKey}" -X GET "${apiUrl}"checks/ > "${monitorsFullFile}" || fatal
     elif [[ ${providerName} == 'upptime' ]]; then
-        curl --fail -s -H "Authorization: bearer ${ghToken}" "${apiUrl}repos/${gitHubUsername}/${upptimeRepo}/git/trees/master?recursive=1" | grep -e 'api\/' | awk -F'/' 'NF==2' | awk -F'/' '{print $2}' | tr -d '",' > "${monitorsFullFile}" || fatal
+        curl --fail -s -H "Authorization: bearer ${ghToken}" "${apiUrl}repos/${repoOwner}/${upptimeRepo}/git/trees/master?recursive=1" | grep -e 'api\/' | awk -F'/' 'NF==2' | awk -F'/' '{print $2}' | tr -d '",' > "${monitorsFullFile}" || fatal
     fi
 }
 
@@ -814,7 +830,7 @@ create_friendly_list() {
             siteURL=$(grep url "${tempDir}${monitor}"_short.txt | awk '{print $2}')
             friendlyName=$(curl --fail -s "${upRawURL}master/.upptimerc.yml" | grep -v href | grep -B1 "${siteURL}"$ | grep name | awk -F':' '{print $2}' | cut -c2- 2> /dev/null) || fatal
             status=$(grep status "${tempDir}${monitor}"_short.txt  | grep -v url | awk '{print $2}' 2> /dev/null) || fatal
-            workflowStatus=$(curl -s -H "Authorization: bearer ${ghToken}" -H "Accept: application/vnd.github.v3+json" "${apiUrl}repos/${gitHubUsername}/${upptimeRepo}/actions/workflows/uptime.yml" | jq -r .state)
+            workflowStatus=$(curl -s -H "Authorization: bearer ${ghToken}" -H "Accept: application/vnd.github.v3+json" "${apiUrl}repos/${repoOwner}/${upptimeRepo}/actions/workflows/uptime.yml" | jq -r .state)
 
             if [[ ${workflowStatus} == 'disabled_manually' ]]; then
                 status='paused'
@@ -860,7 +876,7 @@ get_paused_monitors() {
     true > "${pausedMonitorsFile}"
 
     if [[ ${providerName} == 'upptime' ]]; then
-        workflowStatus=$(curl -s -H "Authorization: bearer ${ghToken}" -H "Accept: application/vnd.github.v3+json" "${apiUrl}repos/${gitHubUsername}/${upptimeRepo}/actions/workflows/uptime.yml" | jq -r .state)
+        workflowStatus=$(curl -s -H "Authorization: bearer ${ghToken}" -H "Accept: application/vnd.github.v3+json" "${apiUrl}repos/${repoOwner}/${upptimeRepo}/actions/workflows/uptime.yml" | jq -r .state)
         if [[ ${workflowStatus} == 'disabled_manually' ]]; then
             while IFS= read -r monitor; do
                 cp "${tempDir}${monitor}".txt "${tempDir}${monitor}"_short.txt
@@ -1014,7 +1030,7 @@ convert_friendly_monitors() {
 pause_all_monitors() {
     if [[ ${providerName} == 'upptime' ]]; then
         echo 'Pausing the Uptime CI workflow for your Upptime repository...'
-        curl --fail -X PUT -H "Authorization: bearer ${ghToken}" -H "Accept: application/vnd.github.v3+json" "${apiUrl}repos/${gitHubUsername}/${upptimeRepo}/actions/workflows/uptime.yml/disable" 2> /dev/null || fatal
+        curl --fail -X PUT -H "Authorization: bearer ${ghToken}" -H "Accept: application/vnd.github.v3+json" "${apiUrl}repos/${repoOwner}/${upptimeRepo}/actions/workflows/uptime.yml/disable" 2> /dev/null || fatal
         echo -e "${grn}Success!${endColor}"
         echo ''
     else
@@ -1124,7 +1140,7 @@ pause_specified_monitors() {
 unpause_all_monitors() {
     if [[ ${providerName} == 'upptime' ]]; then
         echo 'Unpausing the Uptime CI workflow for your Upptime repository...'
-        curl --fail -X PUT -H "Authorization: bearer ${ghToken}" -H "Accept: application/vnd.github.v3+json" "${apiUrl}repos/${gitHubUsername}/${upptimeRepo}/actions/workflows/uptime.yml/enable" 2> /dev/null || fatal
+        curl --fail -X PUT -H "Authorization: bearer ${ghToken}" -H "Accept: application/vnd.github.v3+json" "${apiUrl}repos/${repoOwner}/${upptimeRepo}/actions/workflows/uptime.yml/enable" 2> /dev/null || fatal
         echo -e "${grn}Success!${endColor}"
         echo ''
     else
@@ -1610,7 +1626,7 @@ main() {
                 if [[ ${unpausePrompt} =~ ^(Yes|yes|Y|y)$ ]]; then
                     if [[ ${providerName} == 'upptime' ]]; then
                         echo 'Unpausing the Uptime CI workflow for your Upptime repository...'
-                        curl --fail -s -X PUT -H "Authorization: bearer ${ghToken}" -H "Accept: application/vnd.github.v3+json" "${apiUrl}repos/${gitHubUsername}/${upptimeRepo}/actions/workflows/uptime.yml/enable" || fatal
+                        curl --fail -s -X PUT -H "Authorization: bearer ${ghToken}" -H "Accept: application/vnd.github.v3+json" "${apiUrl}repos/${repoOwner}/${upptimeRepo}/actions/workflows/uptime.yml/enable" || fatal
                         echo -e "${grn}Success!${endColor}"
                         echo ''
                     else
